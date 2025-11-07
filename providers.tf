d0b4b9ed479456599b060fa730bb65baa32bed0a
rossopoloco <rossopoloco@gmail.com>
2025-11-07 07:02:39 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,31 @@
 terraform {
   required_version = ">= 1.6"
-  required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
-  }
+
   cloud {
-    orgnization = "rosso" #TFC里面的org名称
-    workspaces { name = "kaiyi-TFC" }
+    organization = "rosso"       # TFC 左上角 org 名
+    workspaces {
+      name = "kaiyi-TFC"         # 和你刚建的 workspace 名一致
+    }
   }
 
-  providers "aws" {
-    region = var.region
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
 }
+
+variable "region" {
+  type    = string
+  default = "ap-northeast-1"
+}
+
+provider "aws" {
+  region = var.region
+}
+
