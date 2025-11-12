d97955e5edd72cc037a449119c2ddc1f33d3d14d
rossopoloco <rossopoloco@gmail.com>
2025-11-12 07:31:16 +0000

    Update s3.tf
    delete s3

--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
-resource "random_id" "suffix" {
-  byte_length = 4
-}
+#resource "random_id" "suffix" {
+#  byte_length = 4
+#}
 
-resource "aws_s3_bucket" "demo" {
-  bucket = "rosso-kaiyi-tfc-demo-${random_id.suffix.hex}"
-}
+#resource "aws_s3_bucket" "demo" {
+#  bucket = "rosso-kaiyi-tfc-demo-${random_id.suffix.hex}"
+#}
 
-output "bucket_name" {
-  value = aws_s3_bucket.demo.bucket
-}
+#output "bucket_name" {
+#  value = aws_s3_bucket.demo.bucket
+#}
